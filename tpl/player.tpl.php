0f329c7b69982f2dc715fab0c79f909b9246c711
Dennis Eichhorn <spl1nes.com@googlemail.com>
2023-11-03 00:09:14 +0100

    Update player.tpl.php

--- tpl/player.tpl.php
+++ tpl/player.tpl.php
@@ -6,13 +6,33 @@ $playername = $request->getData('name');
 
 $query = new Builder($db);
 $query->raw(
+    'SELECT elo.elo_datetime, elo.elo_elo
+    FROM elo
+    left join driver on elo.elo_driver = driver.driver_id
+    WHERE driver.driver_name = \'' . $playername . '\'
+    ORDER BY elo.elo_datetime ASC;'
+);
+$elos = $query->execute()->fetchAll();
+
+$query = new Builder($db);
+$query->raw(
+    'SELECT glicko.glicko_datetime, glicko.glicko_elo
+    FROM glicko
+    left join driver on glicko.glicko_driver = driver.driver_id
+    WHERE driver.driver_name = \'' . $playername . '\'
+    ORDER BY glicko.glicko_datetime ASC;'
+);
+$glickos = $query->execute()->fetchAll();
+
+$query = new Builder($db);
+$query->raw(
     'SELECT glicko2.glicko2_datetime, glicko2.glicko2_elo
     FROM glicko2
     left join driver on glicko2.glicko2_driver = driver.driver_id
     WHERE driver.driver_name = \'' . $playername . '\'
     ORDER BY glicko2.glicko2_datetime ASC;'
 );
-$elos = $query->execute()->fetchAll();
+$glicko2s = $query->execute()->fetchAll();
 ?>
 
 <script src="/../../Resources/chartjs/chart.js"></script>
@@ -23,15 +43,15 @@ $elos = $query->execute()->fetchAll();
     "data": {
         "labels": [
             <?php
-            echo \implode(',', \array_map(function ($var) {return '"' . \date('Y-m-d', $var['glicko2_datetime']) . '"';}, $elos));
+            echo \implode(',', \array_map(function ($var) {return '"' . \date('Y-m-d', $var['glicko2_datetime']) . '"';}, $glicko2s));
             ?>
         ],
         "datasets": [
             {
-                "label": "<?= \htmlspecialchars($playername); ?>",
+                "label": "Elo",
                 "data": [
                     <?php
-                    echo \implode(',', \array_map(function ($var) {return \sprintf('%02d', $var['glicko2_elo']);}, $elos));
+                    echo \implode(',', \array_map(function ($var) {return \sprintf('%02d', $var['elo_elo']);}, $elos));
                     ?>
                 ],
                 "yAxisID": "y",
@@ -39,6 +59,32 @@ $elos = $query->execute()->fetchAll();
                 "tension": 0.0,
                 "borderColor": "rgb(54, 162, 235)",
                 "backgroundColor": "rgb(54, 162, 235)"
+            },
+            {
+                "label": "Glicko1",
+                "data": [
+                    <?php
+                    echo \implode(',', \array_map(function ($var) {return \sprintf('%02d', $var['glicko_elo']);}, $glickos));
+                    ?>
+                ],
+                "yAxisID": "y",
+                "fill": false,
+                "tension": 0.0,
+                "borderColor": "rgb(46, 204, 113)",
+                "backgroundColor": "rgb(46, 204, 113)"
+            },
+            {
+                "label": "Glicko2",
+                "data": [
+                    <?php
+                    echo \implode(',', \array_map(function ($var) {return \sprintf('%02d', $var['glicko2_elo']);}, $glicko2s));
+                    ?>
+                ],
+                "yAxisID": "y",
+                "fill": false,
+                "tension": 0.0,
+                "borderColor": "rgb(204, 46, 46)",
+                "backgroundColor": "rgb(204, 46, 46)"
             }
         ]
     },
@@ -48,32 +94,22 @@ $elos = $query->execute()->fetchAll();
             "y": {
                 "title": {
                     "display": true,
-                    "text": "Elo"
+                    "text": "Rating"
                 },
                 "display": true,
                 "position": "left"
+            }
+        },
+        "plugins": {
+            "title": {
+                "display": true,
+                "text": "<?= \htmlspecialchars($playername); ?>"
             }
         }
     }
 }
 '
 ></canvas>
-
-<table>
-    <thead>
-        <tr>
-            <td>Date</td>
-            <td>Rating</td>
-        </tr>
-    </thead>
-    <tbody>
-        <?php foreach ($elos as $elo) : ?>
-        <tr>
-            <td><?= $elo['glicko2_datetime']; ?>
-            <td><?= (int) $elo['glicko2_elo']; ?>
-        <?php endforeach; ?>
-    </tbody>
-</table>
 
 <script>
 const chart = document.getElementsByTagName('canvas')[0];
